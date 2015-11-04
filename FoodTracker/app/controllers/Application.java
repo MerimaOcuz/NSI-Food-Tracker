c42af9afc868e44a15c6795eab0d1a2de3fa7449
amradervic <amradervic@gmail.com>
2015-11-04 01:27:58 +0100

    start of registration
    http://localhost:9000/register

--- FoodTracker/app/controllers/Application.java
+++ FoodTracker/app/controllers/Application.java
@@ -8,6 +8,7 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 	static Form<Login> loginForm = form(Login.class);
+	static Form<Register> registerForm = form(Register.class);
 
 	public Result index() {
 		return ok(index.render("Food Tracker Application"));
@@ -16,13 +17,29 @@ public class Application extends Controller {
     public static Result login() {
     	return ok(login.render(loginForm));
     }
+	
+	public static Result register() {
+    	String user = session("username");
+		String role = session("role");
+        return ok(
+        	register.render(user, role, registerForm)
+        	);
+    }
     
     public static class Login {
-
         public String username;
         public String password;
-
     }
+	
+	public static class Register {
+	    public String email;
+	    public String name;
+	    public String surname;
+	    public String password;
+	    public String phone;
+	    public String address;
+	}
+	
     /*
     public static Result authenticate() {
         Form<Login> loginForm = form(Login.class).bindFromRequest();
